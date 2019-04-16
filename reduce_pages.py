24a04d9b0c1b5255db418180aaa4c98296ec079a
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2019-04-16 14:53:56 -0400

    Added documentation

--- reduce_pages.py
+++ reduce_pages.py
@@ -1,7 +1,25 @@
 
 def reduce_page(updated_page, db_page):
-  new_page = {}
+  """Takes in an updated_page and a database_page
+  and returns a merged one"""
+  # If the new page is visited then we need to update the visited info 
+  if hasattr(updated_page, 'visited'):
+    # Use Change to Visited State
+    return _visited_page(updated_page, db_page)
+  else: 
+    # Update state
+    return _update_page(updated_page, db_page)
 
-  if hasattr(db_page, 'visisted'):
-    if db_page['visited']:
-      pass
+
+def _update_page(updated_page, db_page):
+  db_page['links_to_page'] += updated_page['links_to_page']
+  db_page['metadata']['links_to_page'] += updated_page['metadata']['links_to_page']
+  return db_page
+
+
+def _visited_page(updated_page, db_page):
+  # Instead of updating the db entry with all the updates we will 
+  # update the updated page with info from the db_page
+  updated_page['links_to_page'] = db_page['links_to_page'] 
+  updated_page['metadata']['links_to_page'] = db_page['metadata']['links_to_page']
+  return updated_page
