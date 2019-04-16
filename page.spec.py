66e59a5ddd28e267797612dd3312f76312fad8c0
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2019-04-16 14:57:35 -0400

    updated specs

--- page.spec.py
+++ page.spec.py
@@ -1,25 +1,48 @@
 
-page = {
-    "url": {
-        "visited": True, # Not Required, Values T/F
-        "valid": True, # Not Required, Values T/F
-        "links_on_page": ["url's"], # Required
-        "links_to_page": ["url from"],
-        "metadata": {
-            "url": 'url', # This page's url Required
-            "internal_links": 0,# Num Required 
-            "external_links": 0, # Num Required
-            "telephone_nums": 0, # Num Required
-            "links_to_page": 0, # Num Required
-            "url_components": {   
-                "scheme": "http/s",
-                "netloc": "www.regent.edu:80",
-                "netloc_min": "regent.edu",
-                "path": "/home",
-                "params": "params",
-                "query": "query",
-                "fragment": "fragment"
-            }
+db_page = {
+  "_id": "url",
+  "page": {},
+}
+
+page_visited = {
+    "url": 'url', # This page's url Required
+    "visited": True,  # Values T/F, Updated On Visited
+    "valid": True,    # Values T/F, Updated On Visited
+    "links_on_page": ["url's"],   # Updated On Visited
+    "links_to_page": ["url from"],# Not Updated on Visited
+    "tel_on_page": ["telephone numbers"], # Updated on Visited
+    "metadata": {
+        "internal_links": 0, # Num Required, Updated On Visited 
+        "external_links": 0, # Num Required, Updated On Visited
+        "telephone_nums": 0, # Num Required, Updated On Visited
+        "links_to_page": 0,  # Num Required, Updated On Visited
+        "url_components": {   
+            "scheme": "http/s",
+            "netloc": "www.regent.edu:80",
+            "netloc_min": "regent.edu",
+            "path": "/home",
+            "params": "params",
+            "query": "query",
+            "fragment": "fragment",
         }
     }
 }
+
+page_meta_min = {
+  "url": 'url', # This page's url Required
+  "links_to_page": ["url from"], # Updated Every Update Cycle
+  "visited": False, # 
+    "metadata": {
+        "links_to_page": 0, # Num Required Updated Every Update Cycle
+        "url_components": {   
+          "scheme": "http/s",
+          "netloc": "www.regent.edu:80",
+          "netloc_min": "regent.edu",
+          "path": "/home",
+          "params": "params",
+          "query": "query",
+          "fragment": "fragment"
+        }
+    }
+}
+
