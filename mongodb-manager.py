c17fe31e29d769170eb2dd723889e6e4e8aaf2b4
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2019-04-16 14:57:22 -0400

    added lots of cuntionality to the db manager

--- mongodb-manager.py
+++ mongodb-manager.py
@@ -1,4 +1,5 @@
 import pymongo
+from reduce_pages import reduce_page
 
 class MongoDB:
   def __init__(self, base_url):
@@ -13,17 +14,36 @@ class MongoDB:
     # Create / Grab Collections
     self.pages_collection = self.db["pages"]
 
-  def imsert_links(self, url_array):
-
+  def insert_links(self, url_array):
+ 
     for link in url_array:
-      # Check if the link is in the collection
-      if self.pages_collection.count_documents({"_id": link['url']  }) > 0:
+      self.insert_page(link)
+
+
+  def insert_page(self, updated_page):
+    if self.pages_collection.count_documents( { "_id": updated_page['url'] } ) > 0:
         # If the link exists update its data 
-        
 
+        page_tmp = reduce_page(
+          updated_page,
+          self.pages_collection.find_one( { "_id": updated_page['url'] } )
+        )
 
-      else: 
-        # Otherwise create an entry and update its links from 
+        # Replace old page entry in datbase
+        self.pages_collection.replace_one(
+          {"_id": updated_page['url']},
+          page_tmp
+        )
 
+    else:
+        # If the page doesnt 
+        updated_page['visited'] = False
+        page_tmp = {"_id": updated_page['url'],
+                    "page": updated_page}
 
-        
+        # Insert document
+        self.pages_collection.insert_one(page_tmp)
+
+  def return_records(self,num_records):
+    cursor = self.pages_collection.find({"visited": False}, limit=num_records)
+    return cursor
